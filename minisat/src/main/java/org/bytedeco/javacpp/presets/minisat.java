29653b3a1bc81092432038473d9b87d398db76b6
AE <ae@ae.nl>
2016-06-12 10:28:44 +0200

    Greatly reduces minisat complexity.

--- minisat/src/main/java/org/bytedeco/javacpp/presets/minisat.java
+++ minisat/src/main/java/org/bytedeco/javacpp/presets/minisat.java
@@ -32,26 +32,19 @@ public class minisat implements InfoMapper {
                .put(new Info("_Size").translate().valueTypes("int").pointerTypes("int"))
                .put(new Info("Minisat::Lit").cppText("Lit"))
                .put(new Info("Minisat::Var").cast().valueTypes("int").pointerTypes("IntPointer", "IntBuffer", "int[]"))
-               .put(new Info("vec<Minisat::Lit>::Size").translate().valueTypes("int").pointerTypes("int"))
+               .put(new Info("Minisat::vec<Minisat::Lit>::Size").translate().valueTypes("int").pointerTypes("int"))
                .put(new Info("Minisat::IntSet<Minisat::Lit,Minisat::MkIndexLit>").cast().pointerTypes("LitMkIndexLitSet").define())
                .put(new Info("Minisat::vec<Minisat::Var,int>",
                              "Minisat::vec<int,int>",
-                             "vec<Minisat::Var>",
-                             "Minisat::vec<Var,int>",
-                             "vec<Var, int>").pointerTypes("VarVecPointer").define())
+                             "Minisat::vec<Minisat::Var>").pointerTypes("VarVecPointer").define())
                .put(new Info("Minisat::vec<Minisat::lbool,int>",
-                             "Minisat::vec<Minisat::lbool>",
-                             "Minisat::vec<lbool,int>",
-                             "Minisat::vec<lbool>").pointerTypes("LboolVecPointer").define())
+                             "Minisat::vec<Minisat::lbool>").pointerTypes("LboolVecPointer").define())
                .put(new Info("Minisat::vec<Minisat::Lit,int>",
-                              "vec<Minisat::Lit>",
-                              "Minisat::vec<Lit,int>",
-                              "vec<Lit,int>").pointerTypes("LitVecPointer").define())
+                             "Minisat::vec<Minisat::Lit>").pointerTypes("LitVecPointer").define())
                .put(new Info("Minisat::RegionAllocator<uint32_t>::Ref",
                              "RegionAllocator<uint32_t>::Ref",
-                             "Ref").cast().valueTypes("long").pointerTypes("long").define())
+                             "Minisat::Ref").cast().valueTypes("long").pointerTypes("long").define())
                .put(new Info("Minisat::RegionAllocator<uint32_t>").pointerTypes("RegionAllocatorPointer"))
-               .put(new Info("Minisat::ClauseAllocator", "::Minisat::ClauseAllocator").skip())
                .put(new Info("Minisat::ClauseIterator").skip())
                .put(new Info("Minisat::ClauseAllocator").skip())
                .put(new Info("Minisat::Clause").skip())
